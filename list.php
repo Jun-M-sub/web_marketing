d3d81b540cc250ffc5bca4fad423b94deb0e2adf
Jun Maeda <ishida@rinsendo.com>
2019-12-23 14:36:27 +0900

    shosai.phpのcss編集

--- list.php
+++ list.php
@@ -17,7 +17,12 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Example</title>
+    <title>店舗一覧</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -46,25 +51,33 @@
         }
 
     </style>
+    <main>
+        <div class="main-first">
+            <div class="img">
+                <img src="img/image823.png">
+            </div>
+            <h1 class="shosai_h1">申込一覧</h1>
+        </div>
+        <div class="main-inner">
+            <table>
+                <tr class="type06">
+                    <th>名前</th>
+                    <th>申込日</th>
+                    <th>店舗名</th>
+                    <th>詳細</th>
+                </tr>
+                <?php foreach($stmt as $me){ ?>
+                <tr>
+                    <td><?php echo $me['person_name']; ?></td>
+                    <td><?php echo date("Y年m月d日H時i分",strtotime($me['date'])); ?></td>
+                    <td><?php echo $me['shop_name']; ?></td>
+                    <td><a href="shosai.php?id=<?php echo $me['id']; ?>">詳細</a></td>
+                </tr>
+                <?php } ?>
 
-    <table class="">
-        <tr class="type06">
-            <th>名前</th>
-            <th>申込日</th>
-            <th>店舗名</th>
-            <th>詳細</th>
-        </tr>
-        <?php foreach($stmt as $me){ ?>
-        <tr>
-            <td><?php echo $me['person_name']; ?></td>
-            <td><?php echo date("Y年m月d日H時i分",strtotime($me['date']));; ?></td>
-            <td><?php echo $me['shop_name']; ?></td>
-            <td><a href="shosai.php?id=<?php echo $me['id']; ?>">詳細</a></td>
-        </tr>
-        <?php } ?>
-
-    </table>
-
+            </table>
+        </div>
+    </main>
 </body>
 
 </html>
--- shosai.php
+++ shosai.php
@@ -19,92 +19,77 @@ $id = $_GET['id'];
 
 <head>
     <meta charset="utf-8">
-    <title>Example</title>
+    <title>店舗ページ</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <style>
-        table,
-        th,
-        td {
-            border-collapse: collapse;
-            border: 1px solid #ccc;
-            line-height: 1.5;
-        }
 
-        th {
-            width: 150px;
-            padding: 10px;
-            font-weight: bold;
-            vertical-align: top;
-            background: #3f3f3f;
-            color: #ffffff;
-        }
-
-        td {
-            width: 350px;
-            padding: 10px;
-            vertical-align: top;
-        }
-
-    </style>
-
-    <table>
-        <tr>
-            <th>申込日</th>
-            <th>名前</th>
-            <th>電話番号</th>
-        </tr>
-        <tr>
-            <td><?php echo date("Y年m月d日H時i分",strtotime($rec['date'])); ?></td>
-            <td><?php echo $rec['person_name']; ?></td>
-            <td><?php echo $rec['person_phone']; ?></td>
-            
-        </tr>
-    </table>
-    <table>
-        <tr>
-             <th>店舗名前</th>
-            <th>店舗電話番号</th>
-            <th>マイビジネスID</th>
-            <th>サービスカテゴリー</th>
-            <th>郵便番号</th>
-            <th>住所</th>
-        </tr>
-        <tr>
-            <td><?php echo $rec['shop_name']; ?></td>
-            <td><?php echo $rec['shop_phone']; ?></td>
-            <td><?php echo $rec['my_id']; ?></td>
-            <td><?php echo $rec['main_cat']; ?></td>
-            <td><?php echo $rec['zip_code']; ?></td>
-            <td><?php echo $rec['adds']; ?></td>
-        </tr>
-    </table>
-    <div>
-    <div>店舗説明</div>
-    <div><?php echo $rec['description']; ?></div>
+    <main>
+        <div class="main-first">
+            <div class="img">
+            <img src="img/image823.png">
         </div>
-    <table>
-        <tr>
-            <th>カード番号</th>
-            <th>セキュリティコード</th>
-            <th>有効期限</th>
-            <th>カード名義</th>
-        </tr>
-        <tr>
-            <td><?php echo $rec['card_num']; ?></td>
-            <td><?php echo $rec['card_code']; ?></td>
-            <td><?php echo $rec['card_month']; ?>月/ <?php echo $rec['card_year']; ?>年</td>
-            <td><?php echo $rec['card_name']; ?></td>
-        </tr>
-    </table>
-    <?php 
+            <h1 class="shosai_h1"><?php echo $rec['shop_name']; ?></h1>
+        </div>
+        <div class="main-inner">
+            <h2>店舗情報</h2>
+            <dl class="shosai row">
+                <dt class="col-sm-3">店舗電話番号</dt>
+                <dd class="col-sm-9"><?php echo $rec['shop_phone']; ?></dd>
+                <dt class="col-sm-3">マイビジネスID</dt>
+                <dd class="col-sm-9"><?php echo $rec['my_id']; ?></dd>
+                <dt class="col-sm-3">サービスカテゴリー</dt>
+                <dd class="col-sm-9"><?php echo $rec['main_cat']; ?></dd>
+                <dt class="col-sm-3 border-none">店舗説明</dt>
+                <dd class="col-sm-9 border-none"><?php echo $rec['description']; ?></dd>
+
+            </dl>
+
+            <dl class="shosai row">
+                <dt class="col-sm-3">郵便番号</dt>
+                <dd class="col-sm-9"><?php echo $rec['zip_code']; ?></dd>
+                <dt class="col-sm-3 border-none">住所</dt>
+                <dd class="col-sm-9 border-none"><?php echo $rec['adds']; ?></dd>
+            </dl>
+
+            <h2>担当者情報</h2>
+
+            <dl class="shosai row">
+                <dt class="col-sm-3">申込日</dt>
+                <dd class="col-sm-9"><?php echo date("Y年m月d日H時i分",strtotime($rec['date'])); ?></dd>
+                <dt class="col-sm-3">担当者名</dt>
+                <dd class="col-sm-9"><?php echo $rec['person_name']; ?></dd>
+                <dt class="col-sm-3 border-none">担当者番号</dt>
+                <dd class="col-sm-9 border-none"><?php echo $rec['person_phone']; ?></dd>
+            </dl>
+            <h2>クレジットカード情報</h2>
+
+
+
+            <dl class="shosai row">
+                <dt class="col-sm-3">カード番号</dt>
+                <dd class="col-sm-9"><?php echo $rec['card_num']; ?></dd>
+                <dt class="col-sm-3">セキュリティコード</dt>
+                <dd class="col-sm-9"><?php echo $rec['card_code']; ?></dd>
+                <dt class="col-sm-3">有効期限</dt>
+                <dd class="col-sm-9"><?php echo $rec['card_month']; ?>月/ <?php echo $rec['card_year']; ?>年</dd>
+                <dt class="col-sm-3 border-none">カード名義</dt>
+                <dd class="col-sm-9 border-none"><?php echo $rec['card_name']; ?></dd>
+            </dl>
+
+
+            <?php 
     
 $dbh =null;
       
-?>
-
-
+?><div class="btn-inner"><input class="back-confirm-btn" type="button" onclick="history.back()" value="戻る"></div>
+        </div>
+    </main>
 
 </body>
 
