6dd470c0b2b449631381bed2b52677abf04b6deb
Jun Maeda <ishida@rinsendo.com>
2019-12-26 17:20:58 +0900

    ファビコン追加

--- header.php
+++ header.php
@@ -12,10 +12,9 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <!-- ファビコン
-    <link rel="shortcut icon" href="namahage-fav2.png">
-    <link rel="apple-touch-icon" href="namahage-fav2.png">
--->
+<!--     ファビコン-->
+    <link rel="shortcut icon" href="favicon.ico">
+
 
     <!-- スタイルシート
     <link rel="stylesheet" href="css/reset.css">
